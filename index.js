a59629869792007656720691de3af651b358b2aa
Marvin <marvinepoquiz@gmail.com>
2021-11-22 08:01:13 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Your code goes here!!
+
+document.addEventListener('DOMContentLoaded', function (){
+    text = document.getElementById('text')
+    text.textContent = "This is really cool!"
+
+})
